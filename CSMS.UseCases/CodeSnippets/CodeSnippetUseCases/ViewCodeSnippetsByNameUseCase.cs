4ac041902db0cd4a76b6a3512c43768fe9c3d7c2
pjjdebruijn <66751819+pjjdebruijn@users.noreply.github.com>
2022-11-29 23:29:27 +0100

    removed references

--- CSMS.UseCases/CodeSnippets/CodeSnippetUseCases/ViewCodeSnippetsByNameUseCase.cs
+++ CSMS.UseCases/CodeSnippets/CodeSnippetUseCases/ViewCodeSnippetsByNameUseCase.cs
@@ -13,16 +13,8 @@ namespace CSMS.UseCases.UseCases.CodeSnippetUseCases
             this.codeSnippetsRepository = codeSnippetsRepository;
         }
 
-        /// <summary>
-        ///     Executes an Asynchrous operation that returns an IEnumerable<Codesnippets> 
-        ///     and accepts a string name parameter, neccesary to perform the asynchrous operation.
-        ///     poetry in code.
-        /// </summary>
-        /// <param name="name">name parmater used to perform an asynchrous search operation</param>
-        /// <returns>Task<IEnumerable<CodeSnippet>></returns>
         public async Task<IEnumerable<CodeSnippet>> ExecuteAsync(string name = "")
         {
-            // Simple PassThrough
             return await this.codeSnippetsRepository.GetCodeSnippetsByNameAsync(name);
         }
     }
